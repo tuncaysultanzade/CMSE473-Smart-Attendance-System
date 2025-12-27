5d45c32cc2e09d8249825161932eb1401bf23256
tuncaysultanzade <145073199+tuncaysultanzade@users.noreply.github.com>
2025-12-27 02:41:24 +0200

    Update RoleMiddleware.php

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -8,29 +8,19 @@ use Illuminate\Support\Facades\Auth;
 
 class RoleMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string  ...$roles
-     * @return \Illuminate\Http\Response|mixed
-     */
+
     public function handle(Request $request, Closure $next, ...$roles)
     {
-        // Ensure the user is authenticated
         if (!Auth::check()) {
             return redirect()->route('login');
         }
 
         $user = Auth::user();
 
-        // Check if user role is in the allowed roles
         if (in_array($user->user_role, $roles)) {
             return $next($request);
         }
 
-        // Redirect if the user is not authorized
         return redirect()->route('dashboard')->with('error', 'You are not authorized to access this page.');
     }
 }
